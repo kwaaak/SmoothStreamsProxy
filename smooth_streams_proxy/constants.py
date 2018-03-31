0c6192a418bc2b9b333fec51af084b8cade6bdeb
s_Fanous <sherif.fanous@gmail.com>
2018-03-31 19:22:48 -0400

    Fix typo

--- smooth_streams_proxy/constants.py
+++ smooth_streams_proxy/constants.py
@@ -7,7 +7,7 @@ VALID_LOGGING_LEVEL_VALUES = ['DEBUG', 'ERROR', 'INFO', 'TRACE']
 VALID_SMOOTH_STREAMS_PROTOCOL_VALUES = ['hls', 'rtmp']
 VALID_SMOOTH_STREAMS_SERVER_VALUES = ['dap', 'deu', 'deu-de', 'deu-nl', 'deu-nl1', 'deu-nl2', 'deu-nl3', 'deu-nl4',
                                       'deu-nl5', 'deu-uk', 'deu-uk1', 'deu-uk2', 'dna', 'dnae', 'dnae1', 'dnae2',
-                                      'dnae3', 'dnae4', 'dnae6', 'dnaw', 'dnaw1', 'dnaw2', 'dnaw3', 'dnaw4x'
+                                      'dnae3', 'dnae4', 'dnae6', 'dnaw', 'dnaw1', 'dnaw2', 'dnaw3', 'dnaw4'
                                       ]
 VALID_SMOOTH_STREAMS_SERVICE_VALUES = ['view247', 'viewmmasr', 'viewss', 'viewstvn']
 VERSION = '2.5.1'
